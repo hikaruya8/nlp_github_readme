fb6f50d0888566ffc800b663cf02cfdebc6729a2
山田暉 <hikaruy0805@gmail.com>
2018-08-18 23:03:57 +0900

    関数の引き継ぎがうまくいってない

--- nlp_github_readme/star_repository_scraping.py
+++ nlp_github_readme/star_repository_scraping.py
@@ -9,6 +9,19 @@ def scraping_star_repo():
 
   star_repo = soup.find_all("div", attrs={"class":"d-inline-block mb-1"}) #starされたリポジトリ抽出
   for link in star_repo:
-    print(link.a.get('href'))
+    repo_link = link.a.get('href')
+    repo_github_link = 'https://github.com/hikaruya8{}'.format(repo_link)
+    return repo_github_link
+
+def scraping_ptag():
+  r = requests.get(target_url2)         #requestsを使って、webから取得
+  soup = BeautifulSoup(r.text, 'lxml') #要素を抽出
+
+  readme_text = soup.find("article", attrs={"class":"markdown-body"}) #readme抽出
+  readme_p = readme_text.find_all("p") #readme内のpタグテキスト全部抽出
+  for p in readme_p:
+      print(p.text)
 
 scraping_star_repo()
+target_url2 = scraping_star_repo()
+scraping_ptag()
